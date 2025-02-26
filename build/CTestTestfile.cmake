026765d1ca36d78431c8daf14789dc8d0e7f5e77
Jane Qin <1139804380@qq.com>
2025-02-27 00:11:40 +0800

    Remove ignored files from Git

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/janeqin/code/lidar_img_syn_ws/src
-# Build directory: /home/janeqin/code/lidar_img_syn_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("sensor_syn/lidar_img_syn_lmx")
--- build/sensor_syn/lidar_img_syn_lmx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/janeqin/code/lidar_img_syn_ws/src/sensor_syn/lidar_img_syn_lmx
-# Build directory: /home/janeqin/code/lidar_img_syn_ws/build/sensor_syn/lidar_img_syn_lmx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
